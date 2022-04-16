6e60ea5227c11a03d4ddaced3aa4c2f99658a0d5
Faye24 <fayemarieoscobido@gmail.com>
2022-04-16 15:10:20 +0800

    My changes

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+
+
+
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +12,82 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  contactlist  = [
+    {id: 1, name: 'Faye Marie Oscobido', email: 'fayemarie@gmail.com'  ,number: '09517833231'},
+
+   
+  ]
+
+
+
+
+  constructor(public alertController: AlertController ) {
+  }
+  async  confirmation(index: number) {
+    const alert = await this.alertController.create({
+      header: 'Are You Sure You Want To Delete?',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          handler: () => {
+              this.contactlist.splice(index, 1);
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel', 
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ],
+    });
+
+    await alert.present();
+  }
+  async addUser() {
+    let prompt = await this.alertController.create({
+      header: 'Add Contact',
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'name',
+        },{
+          name: 'email',
+          placeholder: 'email'
+        },
+        {
+          name: 'number',
+          placeholder: 'number'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: (data) => {
+            console.log('Saved clicked');
+            
+          this.contactlist.push({
+            id: data.id,
+            name: data.name,
+            email: data.email,
+            number: data.number
+          });
+     
+          }
+        }
+      ]
+    });
+    await prompt.present();
+
+  }
+
 
 }
